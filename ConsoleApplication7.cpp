574887baa4d25bc821c0486f264598e1f7f311ca
zamadshakil <146313727+zamadshakil@users.noreply.github.com>
2024-12-22 10:25:03 +0500

    Add project files.

---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,89 @@
+//Below is a square type pattern of stars
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    int n;
+//    cout << "Enter number for lenght of pattern: ";
+//    cin >> n;
+//
+//    for (int i = 1; i <= n; i++)
+//    {
+//        for (int j = 1; j <= n; j++) {
+//            cout << "* ";
+//        }
+//        cout << endl;
+//    }
+//    return 0;
+//}
+//----------------------------------------------------------------------------------------------------------------------------
+
+
+
+//Below is left side pyramid pattern of stars
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int n;
+//	cout << "Enter number for pattern lenght.";
+//	cin >> n;
+//	for (int i = 1; i <= n; i++) {
+//		for (int j = 1; j <= i; j++) {
+//			cout << "*";
+//		}
+//		cout << endl;
+//
+//	}
+//	return 0;
+//}
+
+
+//----------------------------------------------------------------------------------------------------------------------------
+ 
+ 
+// Below is right side pyramid pattern of stars
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int n;
+//	cout << "Enter number for pattern lenght: ";
+//	cin >> n;
+//	int veri = n;
+//	for (int i = 1; i <= n; i++) {
+//		for (int j = 1; j <= (veri-1); j++) {
+//			cout << " ";
+//		}
+//		for (int k = 1; k <= i; k++) {
+//			cout << "*";
+//		}
+//		veri--;
+//		cout << endl;
+//	}
+//	return 0;
+//}
+
+
+//----------------------------------------------------------------------------------------------------------------------------
+
+//Below is pyramid pattern of stars
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cout << "Enter number for pattern lenght: ";
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= (n - 1); j++) {
+			cout << " ";
+		}
+		for(int k =1; k<)
+	}
+}
