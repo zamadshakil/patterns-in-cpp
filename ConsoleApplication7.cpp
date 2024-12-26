02c9bd916f6ad8258d1cfaf86e5ccff0776d5565
Zamad Shakeel <146313727+zamadshakil@users.noreply.github.com>
2024-12-26 16:20:28 +0500

    graded lab 3 added Update ConsoleApplication7.cpp

--- ConsoleApplication7.cpp
+++ ConsoleApplication7.cpp
@@ -146,5 +146,33 @@ int main() {
     return 0;
 }
 	*/
+
+/*lab work
+	 #include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "Enter a number: ";
+	cin >> n;
+	int decr = n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= decr; j++) {
+			cout << " ";
+		}
+		for (int k = 1; k <= i; k++) {
+			cout << k;
+		}
+		for (int m = i-1; m >= 1; m--) {
+			cout << m;
+		}
+		cout << endl;
+		decr--;
+	}
+
+	return 0;
+}
+*/
 	return 0;
 }
