9a573da234410e712cdc8d2b8ebaaa01fb7be764
Zamad Shakeel <146313727+zamadshakil@users.noreply.github.com>
2024-12-26 13:38:03 +0500

    Update ConsoleApplication7.cpp added a hollow diamond shape
    outer sides of hollow diamonds will be empty

--- ConsoleApplication7.cpp
+++ ConsoleApplication7.cpp
@@ -91,5 +91,55 @@ int main() {
 		veri--;
 		cout << endl;
 	}
+
+//hollow diamond shape in cpp
+	/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n; 
+    cin>>n;
+    int s1_incr=1, decr1=n; 
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=decr1; j++){
+            cout<<"*";
+        }
+        
+        for(int k=1; k<=s1_incr; k++){
+            cout<<" ";
+        }
+        for(int l=1; l<s1_incr; l++){
+            cout<<" ";
+        }
+        for(int m=1; m<=decr1; m++){
+            cout<<"*";
+        }
+        cout<<endl;
+        s1_incr++;
+        decr1--;
+        
+    }
+    int incr2=1, decr2=n; 
+    for(int a=1; a<=n; a++){
+        for(int b=1; b<=incr2; b++ ){
+            cout<<"*";
+        }
+        for(int c=1; c<=decr2; c++ ){
+            cout<<" ";
+        }
+        for(int d=1; d<decr2; d++){
+            cout<<" ";
+        }
+        for(int f=1; f<=incr2; f++){
+            cout<<"*";
+        }
+        cout<<endl;
+        decr2--;
+        incr2++;
+    }
+    return 0;
+}
+	*/
 	return 0;
 }
